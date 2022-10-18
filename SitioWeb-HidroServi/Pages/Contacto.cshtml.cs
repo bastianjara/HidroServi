b5e043f38b532c4b37a0c11d605b1660e1e1bcf9
bastian jara cumian <bastian.jara@grupomok.com>
2022-10-17 21:21:58 -0300

    :rocket: initial commit

---
+++ SitioWeb-HidroServi/Pages/Contacto.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SitioWeb_HidroServi.Pages
+{
+    public class ContactoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ SitioWeb-HidroServi/Pages/Servicio.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SitioWeb_HidroServi.Pages
+{
+    public class ServicioModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
